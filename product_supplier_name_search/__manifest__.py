aaf3c5d5d794b870b3bcc17af69c7d4c03716bf1
David Dufresne <david.dufresne@numigi.net>
2018-09-07 12:28:55 -0400

    Add module product_supplier_name_search

---
+++ product_supplier_name_search/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Product Supplier Name Search',
+    'version': '11.0.1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Product',
+    'summary': 'Search a product by the supplier reference.',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
