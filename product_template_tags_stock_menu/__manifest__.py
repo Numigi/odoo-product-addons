1c34914fadcf331732b0f58e12c3e1951faed762
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-08-26 11:51:20 -0400

    Add module product_template_tags_stock_menu (#14)
    * Add module product_template_tags_stock_menu
    
    * Fix docker-compose

---
+++ product_template_tags_stock_menu/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Product Template Tag Stock Menu',
+    'version': '11.0.1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Stock',
+    'summary': 'Add menu inventory app for product template tags.',
+    'depends': [
+        'stock',
+        'product_template_tags',
+    ],
+    'data': [
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
