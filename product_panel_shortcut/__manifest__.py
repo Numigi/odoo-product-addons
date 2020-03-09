69f58e6f612aeb027d3d833c213d7b501ddb2034
Jordi Riera <jordi.riera@numigi.com>
2020-03-09 16:03:10 -0400

    add product_panel_shortcut (TA#20121)

---
+++ product_panel_shortcut/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2019 - Today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Product Panel Shortcut',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Product',
+    'summary': 'Add an app icon for the list of products.',
+    'depends': [
+        'account',
+        'stock',
+        'sales_team',
+        'purchase',
+    ],
+    'data': [
+        'menu.xml',
+    ],
+    'installable': True,
+}
