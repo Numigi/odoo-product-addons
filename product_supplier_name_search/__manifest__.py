7239398da6c40cbb2f993a857306c321de3c0758
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-05 14:56:03 -0500

    Update module version

--- product_supplier_name_search/__manifest__.py
+++ product_supplier_name_search/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Product Supplier Name Search',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
