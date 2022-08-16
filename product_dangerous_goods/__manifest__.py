9a567208cb79241b5cf9e1f85a722751c2f34953
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-08-16 14:25:25 +0100

    TA#43870 Add hazard class 3 (8) (#41)

--- product_dangerous_goods/__manifest__.py
+++ product_dangerous_goods/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Dangerous Goods",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://www.numigi.com",
