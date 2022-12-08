28a30060356c47f32ea3b2e64a49e7b29f56088c
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2022-12-08 18:12:13 +0100

    product_kit  ported

--- product_kit/tests/test_product.py
+++ product_kit/tests/test_product.py
@@ -52,5 +52,5 @@ class TestProduct(common.SavepointCase):
         assert line.uom_id == self.uom_unit
 
     def test_copy(self):
-        product = self.product.copy()
-        assert len(product.kit_line_ids) == 1
+        product_tmpl = self.product.product_tmpl_id.copy()
+        assert len(product_tmpl.kit_line_ids) == 1
