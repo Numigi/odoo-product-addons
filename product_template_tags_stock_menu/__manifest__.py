ff21aa4c6e8f123ce4457a743e9f9095b5263139
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2022-12-08 18:58:52 +0100

    [MIG] product_template_tags_stock_menu: Migration to 14.0

--- product_template_tags_stock_menu/__manifest__.py
+++ product_template_tags_stock_menu/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Product Template Tag Stock Menu',
-    'version': '11.0.1.0.0',
+    'version': "14.0.1.0.0",
     'author': 'Numigi',
     'maintainer': 'numigi',
     'website': 'https://bit.ly/numigi-com',
