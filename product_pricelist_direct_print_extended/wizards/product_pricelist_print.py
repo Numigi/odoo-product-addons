b04e88901f02181ea4e2108ce8e01fc84da0396a
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-03-01 21:57:36 +0300

    TA#50768 [MIG] Migration from 12.0 to 14.0 version (#51)
    * TA#50768 [MIG] Migration from 12.0 to 14.0 version
    
    * TA#50768 [FIX][MIG] Unused import and variable

--- product_pricelist_direct_print_extended/wizards/product_pricelist_print.py
+++ product_pricelist_direct_print_extended/wizards/product_pricelist_print.py
@@ -1,14 +1,13 @@
 # © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class ProductPricelistPrint(models.TransientModel):
     _name = 'product.pricelist.print'
     _inherit = ['product.pricelist.print', 'mail.thread']
 
-    @api.multi
     def send_batch(self):
         self.ensure_one()
         for partner in self.partner_ids:
