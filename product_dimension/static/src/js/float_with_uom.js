52f94c7dccf5d637ff8ff46959aee6149a296230
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-05-03 01:12:36 -0400

    TA#53154 [FIX] product_dimension Js V14 Compatibility (#53)

--- product_dimension/static/src/js/float_with_uom.js
+++ product_dimension/static/src/js/float_with_uom.js
@@ -1,13 +1,14 @@
 /*
-    © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+    © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
     License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
 */
 odoo.define("product_dimension.float_with_uom", function(require) {
     "use strict";
 
+    var fields = require("web.basic_fields");
     var registry = require("web.field_registry");
 
-    var FloatWithUoM = registry.get("float").extend({
+    var FloatWithUoM = fields.FieldFloat.extend({
         /**
          * Setup the related uom field attribute.
          */
@@ -44,4 +45,7 @@ odoo.define("product_dimension.float_with_uom", function(require) {
     });
 
     registry.add("float_with_uom", FloatWithUoM);
+    return {
+        FloatWithUoM: FloatWithUoM,
+    };
 });
