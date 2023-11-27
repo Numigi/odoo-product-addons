54db49d8f7eb477bc1104ba0788f0d58d990a3b6
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-11-27 18:51:26 +0300

    TA#57862 [ADD][CHG] product_manufacturer_quick_search : features and view change (#60)

--- product_manufacturer_quick_search/__manifest__.py
+++ product_manufacturer_quick_search/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Manufacturer Quick Search",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
