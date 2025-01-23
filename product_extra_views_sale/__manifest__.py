3aa693b654608ba585e4ede44df07f83acfa6618
Rivo Lalaina <rivo2302@gmail.com>
2025-01-23 14:15:55 +0300

    [16.0][MIG] product_extra_views_sale

---
+++ product_extra_views_sale/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Product Extra Views / Sales',
+    'version': "16.0.1.0.0",
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Product',
+    'summary': 'Add pivot and graph views to products from sales',
+    'depends': [
+        # 'product_extra_views',
+        'sale_management',
+    ],
+    'data': [
+        'views/ir_actions_data.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
